8d4c7b667e3c9e5400bb00f6fd99b2c7a12ff164
Jean Souza <jeanhrsouza@hotmail.com>
2021-02-24 21:00:37 -0300

    Aula 02 - Banco de Dados

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,26 +1,15 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database';
+import { router } from './routes';
 
 const app = express();
 
-/**
- * GET -> BUSCAR
- * POST -> SALVAR
- * PUT -> ALTERAR
- * DELETE -> DELETAR
- * PATCH -> ALTERAR INFORMAÇÃO
- */
+app.use(express.json());
+app.use(router);
 
-app.get("/", (request, response) => {
-    return response.json({message: "Hello World - NLW 04"});
-});
-
-
-app.post("/", (request, response) => {
-    //Recebeu os dados para salvar
-    return response.json({message: "Os dados foram salvos com sucesso!"});
-})
 
 app.listen('3333', () => {
     console.log("Server is running! 🎉");
-    
+
 }); 
